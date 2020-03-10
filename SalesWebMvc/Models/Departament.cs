d8427e25c72eca0776bb390b40057a5f9821071f
gavinhos60 <gavinhos60@gmail.com>
2020-03-10 16:09:10 -0300

    Lista

---
+++ SalesWebMvc/Models/Departament.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class Departament
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
